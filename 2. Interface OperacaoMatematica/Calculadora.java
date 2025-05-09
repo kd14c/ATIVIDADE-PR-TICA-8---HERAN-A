64d236eda2ae30b8d12a09e76f69044989020d25
kd14c <carlos.campolongo@fatec.sp.gov.br>
2025-05-08 23:50:52 -0300

    Add files via upload

---
+++ 2. Interface OperacaoMatematica/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora implements OperacaoMatematica {
+
+    @Override
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
---
+++ 2. Interface OperacaoMatematica/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+public interface OperacaoMatematica {
+    int somar(int a, int b);
+    int subtrair(int a, int b);
+    int multiplicar(int a, int b);
+    int dividir(int a, int b);
+}
